b0f80c6e156d6d1c91a40445b50cf4815cb98fdb
Tim Meusel <tim@bastelfreak.de>
2016-07-15 01:00:17 +0200

    reorder simplecov code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
+require 'simplecov'
 require 'coveralls'
 include RspecPuppetFacts
-Coveralls.wear!
 
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter '/spec/fixtures'
 end
+Coveralls.wear!
