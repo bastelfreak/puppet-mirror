5869f983a426b4d8838076ea76558264d37ee440
Tim Meusel <tim@bastelfreak.de>
2016-07-20 09:50:18 +0200

    add codeclimate to spec runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 require 'simplecov'
 require 'coveralls'
+require 'codeclimate-test-reporter'
 include RspecPuppetFacts
 
 # https://coveralls.zendesk.com/hc/en-us/articles/201769485-Ruby-Rails
@@ -10,3 +11,5 @@ SimpleCov.start do
   add_filter '/spec/fixtures'
 end
 # Coveralls.wear!
+
+CodeClimate::TestReporter.start
